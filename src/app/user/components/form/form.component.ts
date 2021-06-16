96e2e474ad8f422c576e05d63e23958cdde0f424
brahyan <tauronro@gmail.com>
2021-06-16 14:24:58 -0500

    componentes form y list

--- src/app/user/components/form/form.component.ts
+++ src/app/user/components/form/form.component.ts
@@ -1,15 +1,61 @@
+import { UserService } from './../../user.service';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
+  form: FormGroup;
+  id: string | null = null;
+  constructor(private userSvc: UserService, route: ActivatedRoute) {
+    console.log(route.snapshot.params?.id);
 
-  constructor() { }
+    this.form = new FormGroup({
+      id: new FormControl(null),
+      name: new FormControl(''),
+      lastname: new FormControl(''),
+      document_id: new FormControl(''),
+      email: new FormControl(''),
+      phone: new FormControl(''),
+    });
 
-  ngOnInit(): void {
+    if (route.snapshot.params?.id) {
+      this.id = route.snapshot.params.id as string;
+
+      this.userSvc
+        .getById(this.id)
+        .pipe(take(1))
+        .subscribe(({ name, lastname, email, phone, document_id }) => {
+          this.form.patchValue({
+            name,
+            lastname,
+            email,
+            document_id,
+            phone,
+            id: this.id
+          });
+        });
+    }
   }
 
+  ngOnInit(): void {}
+
+  onSave(event: Event) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    if (this.form.valid) {
+      this.userSvc.save(this.form.value).then(() => {
+        console.log('saved');
+      });
+      console.log(this.form.value);
+    } else {
+      console.log('error');
+    }
+  }
 }
--- src/app/user/components/list/list.component.ts
+++ src/app/user/components/list/list.component.ts
@@ -1,15 +1,31 @@
+import { Observable } from 'rxjs';
+import { UserModel } from './../../models/user.model';
+import { UserService } from './../../user.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
+  users$: Observable<UserModel[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private userSvc: UserService) {
+    this.users$ = userSvc.getAll();
   }
 
+  ngOnInit(): void {}
+
+  onDelete(event: Event, user: UserModel) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (confirm(`Seguro que desea eliminar a ${user.name} ${user.lastname}?`)) {
+      this.userSvc.delete(user.id as string).then(()=>{
+        alert('Se eliminó correctamente')
+      })
+    }
+
+  }
 }
